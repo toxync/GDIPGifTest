17c00856f3a766fcbb4041e7a1199285c83152ff
bac6536 <bac6536@naver.com>
2024-01-20 02:22:21 +0900

    remove unused files

--- GDIPGifTest/TXYC_CAPTION_GDIP.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "resource.h"
-#include "TXYC_ListBox.h"
-#include "TXYC_GDIP.h"
-#include "TXYC_CaptionControl.h"
-#include "TXYC_CustomCaption.h"
-#include "TXYC_CustomProgress.h"
